c06f002c3dc3b28cfbb417218e9eae259ac29842
paganyi <paganyi@gmail.com>
2017-06-20 18:32:18 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -8,7 +8,7 @@ import telebot
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message)
+def repeat_all_messages(message):
 bot.send_message(message.chat.id, message.text)
 if__name__ == '__main__':
 bot.polling(none_stop=True)
