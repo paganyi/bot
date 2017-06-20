2db80f4e9815ea42db5d09e4201310b0d74750aa
paganyi <paganyi@gmail.com>
2017-06-20 17:58:31 +0300

    Update and rename README.md to Bot.py

---
+++ Bot.py
@@ -0,0 +1,14 @@
+#-*-coding:utf8;-*-
+#qpy:2
+#qpy:console
+
+import config
+import telebot
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message)
+bot.send_message(message.chat.id, message.text)
+if__name__ == '__main__':
+bot.polling(none_stop=True)
